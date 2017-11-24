a54dac6c08075c4014e14fa9bc1ab9277dc53eb4
hp <hp@12983129837>
2017-11-24 11:59:03 +0630

    Category

---
+++ Project/src/com/soft/Category.java
@@ -0,0 +1,40 @@
+package com.soft;
+
+public class Category {
+	private String foodname;
+	private String foodproduct;
+	private double foodprice;
+	private String foodexpire;
+
+	public String getFoodname() {
+		return foodname;
+	}
+
+	public void setFoodname(String foodname) {
+		this.foodname = foodname;
+	}
+
+	public String getFoodproduct() {
+		return foodproduct;
+	}
+
+	public void setFoodproduct(String foodproduct) {
+		this.foodproduct = foodproduct;
+	}
+
+	public double getFoodprice() {
+		return foodprice;
+	}
+
+	public void setFoodprice(double foodprice) {
+		this.foodprice = foodprice;
+	}
+
+	public String getFoodexpire() {
+		return foodexpire;
+	}
+
+	public void setFoodexpire(String foodexpire) {
+		this.foodexpire = foodexpire;
+	}
+}
