98b8307b2c69751c6343dabe6170af4243f774bd
hp <hp@12983129837>
2017-11-24 12:59:06 +0630

    movies name

---
+++ Project1/src/com/soft/Movies.java
@@ -0,0 +1,31 @@
+package com.soft;
+
+public class Movies {
+	private String moviename;
+	private int moveproduce;
+	private String movietime;
+
+	public String getMoviename() {
+		return moviename;
+	}
+
+	public void setMoviename(String moviename) {
+		this.moviename = moviename;
+	}
+
+	public int getMoveproduce() {
+		return moveproduce;
+	}
+
+	public void setMoveproduce(int moveproduce) {
+		this.moveproduce = moveproduce;
+	}
+
+	public String getMovietime() {
+		return movietime;
+	}
+
+	public void setMovietime(String movietime) {
+		this.movietime = movietime;
+	}
+}
